b5ee06ab494a2a6732de2739c96f225fabe58310
malviynjali11 <anjalimalviya061@gmail.com>
2025-11-11 18:47:12 +0530

    covered till chatbook proj

--- 3-OOPS_in_Python/oops_proj.py
+++ 3-OOPS_in_Python/oops_proj.py
@@ -11,15 +11,17 @@ class ChatBook:
                            2. Press 2 to SignIn
                            3. Press 3 to Write a Post
                            4. Press 4 to message a friend
-                           5. Press any other key to Exit""")
+                           5. Press any other key to Exit
+                           
+                           -> """)
         if user_input == '1':
             self.SignUp() 
         elif user_input == '2':
             self.SignIn()
         elif user_input == '3':
-            pass
+            self.my_post()
         elif user_input == '4':
-            pass
+            self.message_friend()
         else:
             exit()
 
@@ -46,4 +48,22 @@ class ChatBook:
         print("\n")
         self.menu()
 
-obj = ChatBook()
+    def my_post(self):
+        if self.loggedin == True:
+            post = input("Write your post here : ")
+            print(f"Your post '{post} hass been published successfully !")
+        else:
+            print("Please SignIn to write a post.")
+        print("\n")
+        self.menu()
+    def message_friend(self):
+        if self.loggedin == True:
+            friend = input("Enter your friend's username : ")
+            message = input("Enter your message : ")
+            print(f"Your message '{message}' has been sent to {friend} successfully !")
+        else:
+            print("Please SignIn to message a friend.")
+        print("\n")
+        self.menu()
+
+user1 = ChatBook()
